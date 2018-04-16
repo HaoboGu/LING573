5c50bdce3764ca93809a7d010224efec44903c67
Haobo Gu <haobogu@outlook.com>
2018-04-16 13:10:47 -0700

    Update code structure for connectivity

--- src/content_realization.py
+++ src/content_realization.py
@@ -7,6 +7,7 @@
     Python version: 3.4.3
 """
 import operator
+from src.writer import write
 
 
 class ContentRealization:
@@ -17,9 +18,10 @@ class ContentRealization:
         self.scu_type = scu_type
         self.max_length = max_length
 
-    def cr(self, scu):
+    def get_summarizations(self, scu):
         """
-        :param scu: list of summary content units, which have salience scores feature
+        Generate summaries based on score of sentences
+        :param scu: list of summary content units, which have score feature
         :return summary: list of summarized sentences
         """
         # Temporary take scu as sentence extracted, return sentence obj with highest salience score
@@ -35,16 +37,40 @@ class ContentRealization:
                     total_length += len(item.sent)
                 else:
                     break
+
             return summary
 
+    def cr(self, doc_set):
+        """
+        Process every doc in the docset and write the result to corresponding output file
+        :type doc_set: class document
+        """
+        # Overwrite previous result in the first run
+        doc = doc_set.documentCluster()[0]
+        summary = self.get_summarizations(doc.sentences())
+        write(summary, doc.topicID(), over_write=True)
+
+        # Process other docs in docset
+        for doc in doc_set.documentCluster()[1:]:
+
+            summary = self.get_summarizations(doc.sentences())
+            write(summary, doc.topicID(), over_write=False)
+
+    def cr_for_single_doc(self, scu, topic_id):
+        """
+        Process a single doc and write the result to corresponding output file
+        :param scu: list of summary content units, which have score feature
+        :param topic_id: topic id of this doc
+        """
+        summary = self.get_summarizations(scu)
+        write(summary, topic_id, over_write=False)
 
 # Test script
-
-
 # class SCU:
 #     def __init__(self, sent, score):
 #         self.sent = sent
 #         self.score = score
+#
 #
 # scus = []
 # for i in range(0, 10):
@@ -54,8 +80,8 @@ class ContentRealization:
 #     scus.append(scu)
 #
 # content_realization = ContentRealization()
-# re = content_realization.cr(scus)
-# print(re)
+# content_realization.cr_for_single_doc(scus, 'D1010A')
+
 
 
 
--- src/writer.py
+++ src/writer.py
@@ -1,51 +1,60 @@
 #! /usr/bin/python
 """
-    This is a writer module which writes required output to file.
+    This is a writer which contains methods write required output to file.
     The output requirement can be found on Canvas.
 
     Author: Haobo Gu
     Date created: 04/14/2018
+    Python version: 3.4.3
 """
 import os
 
 
-class Writer:
+def write(sentences, topic_id, over_write=True):
     """
-    Take a list of strings as the input and write them to the output file.
-    The output file should have one sentence per line.
-    The output filename is generated using topic id.
+    Write sentences to output file.
+    :param sentences: summarization result
+    :param topic_id: topic id for the summaries
+    :param over_write: choose if overwrite the previous result
+    :type sentences: list[str]
+    :type topic_id: str
+    :type over_write: bool
     """
-    def __init__(self, sentences, topic_id):
-        """
-        Initialization
-        :param sentences: a list of output sentences
-        :param topic_id: topic id for the sentences
-        """
-        self.sents = sentences
-        self.topic_id = topic_id
+    output_filename = generate_output_filename_from_topic_id(topic_id)
+    if over_write:
+        mode = 'w'
+    else:
+        mode = 'a'  # append to file
+    with open(output_filename, mode) as output_file:
+        # Write all sentences
+        for sentence in sentences:
+            output_file.write(sentence+'\n')
 
-    def write(self):
-        """
-        Write sentences to output file. If there is previous result, overwrite.
-        """
-        output_filename = os.path.join('outputs', self.generate_output_filename_from_topic_id())
-        with open(output_filename, 'w') as output_file:
-            # Write all sentences
-            for sentence in self.sents:
-                output_file.write(sentence+'\n')
 
-    def generate_output_filename_from_topic_id(self):
-        """
-        Read topic_id, generate output filename.
-        Topic_id should end with A or B, which is the docset_indicator.
-        :return: output_filename, end with team number
-        """
-        suffix = self.topic_id[-1]
-        # Check the docset indicator
-        if suffix == "A" or "B":
-            id_part1 = self.topic_id[:-1]
-        else:
-            print("docset_indicator should be either A and B")
+def generate_output_filename_from_topic_id(topic_id):
+    """
+    Generate output filename according to the requirement on Canvas
+    :param topic_id: output filename contains topic_id information
+    :type topic_id: str
+    :return: output_filename, end with team number
+    """
+    id_part1 = topic_id[:-1]
+    id_part2 = topic_id[-1]
+    if 'outputs' in os.listdir('.'):
+        path = 'outputs'
+    elif 'outputs' in os.listdir('..'):
+        path = '../outputs'
+    else:
+        print('cannot find output folder, store result in current folder')
+        path = ''
+    # our team number is 1
+    return os.path.join(path, id_part1 + "-A.M.100." + id_part2 + '.1')
 
-        # our team number is 1
-        return id_part1 + "-A.M.100." + suffix + '.1'
+
+# Test script
+# topic = "D0901H"
+# topic2 = "D0808A"
+# sents = ['1', '3', 'a', '4533333']
+# write(sents, topic)
+# write(sents, topic2)
+
