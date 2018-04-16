b6df78fbcc3ce0cd5b565f300c415398abd62549
Haobo Gu <haobogu@outlook.com>
2018-04-15 21:18:45 -0700

    Update CR class and add simple test script

--- src/content_realization.py
+++ src/content_realization.py
@@ -4,7 +4,7 @@
 
     Author: Haobo Gu
     Date created: 04/14/2018
-    Python version: 3.6.2
+    Python version: 3.4.3
 """
 import operator
 
@@ -12,15 +12,12 @@ import operator
 class ContentRealization:
     """
     process method take summary content units(scu) as param and return summarized result
-    class SCU:
-    def __init__(self, sent, score):
-        self.sent = sent
-        self.score = score
     """
-    def __init__(self):
-        self.scu_type = "sentence"
+    def __init__(self, scu_type="sentence", max_length=100):
+        self.scu_type = scu_type
+        self.max_length = max_length
 
-    def process(self, scu):
+    def cr(self, scu):
         """
         :param scu: list of summary content units, which have salience scores feature
         :return summary: list of summarized sentences
@@ -33,7 +30,7 @@ class ContentRealization:
             summary = []
             for item in sorted_scu:
                 # Total length of summary should not larger than 100
-                if total_length + len(item.sent) <= 100:
+                if total_length + len(item.sent) <= self.max_length:
                     summary.append(item.sent)
                     total_length += len(item.sent)
                 else:
@@ -41,7 +38,24 @@ class ContentRealization:
             return summary
 
 
+# Test script
 
+
+# class SCU:
+#     def __init__(self, sent, score):
+#         self.sent = sent
+#         self.score = score
+#
+# scus = []
+# for i in range(0, 10):
+#     sent = str(i)*(10+i)
+#     score = i
+#     scu = SCU(sent, score)
+#     scus.append(scu)
+#
+# content_realization = ContentRealization()
+# re = content_realization.cr(scus)
+# print(re)
 
 
 
