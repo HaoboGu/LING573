b0ffb99ac2ca0b7fdecf194a3686350bebd80fd9
HaoboGu <haobogu@outlook.com>
2018-04-21 00:29:40 -0700

    Add shell scripts

---
+++ run.cmd
@@ -0,0 +1,9 @@
+executable = run.sh
+getenv     = true
+output     = stdout
+notification = never
+arguments = "/dropbox/17-18/573/Data/Documents/training/2009/UpdateSumm09_test_topics.xml /dropbox/17-18/573/AQUAINT /dropbox/17-18/573/AQUAINT-2/data /dropbox/17-18/573/Data/models/training/2009"
+transfer_executable = false
+request_memory = 2*1024
+queue
+
--- src/main.py
+++ src/main.py
@@ -8,20 +8,27 @@ import src.content_realization as cr
 
 
 if __name__ == "__main__":
+    use_local_file = False
+    # Paths and variables
+    if use_local_file:
+        data_home = "/dropbox/17-18/573"
+        demo_training_corpus_file = "./Data/UpdateSumm09_demo_test_topics.xml"
+        training_corpus_file = data_home + "/Data/Documents/training/2009/UpdateSumm09_test_topics.xml"
+        aqua = data_home + "/AQUAINT"
+        aqua2 = data_home + "/AQUAINT-2/data"
+        human_judge = data_home + "/Data/models/training/2009"
+    else:
+        training_corpus_file = sys.argv[1]
+        aqua = sys.argv[2]
+        aqua2 = sys.argv[3]
+        human_judge = sys.argv[4]
     print('Start...')
 
-    # Paths and variables
-    data_home = "."
-    training_corpus_file = data_home + "/Data/Documents/training/2009/UpdateSumm09_test_topics.xml"
-    demo_training_corpus_file = data_home + "/Data/Documents/training/UpdateSumm09_demo_test_topics.xml"
-    aqua = data_home + "/AQUAINT"
-    aqua2 = data_home + "/AQUAINT-2/data"
-    human_judge = data_home + "/Data/models/training/2009"
     comp_rate = 0.1  # The number of sentence selected
     converge_standard = 0.001  # Used to judge if the score is converging
 
     print("Reading Corpus...")
-    training_corpus = dp.generate_corpus(demo_training_corpus_file, aqua, aqua2, human_judge)
+    training_corpus = dp.generate_corpus(training_corpus_file, aqua, aqua2, human_judge)
     docset_list = training_corpus.docsetList()
     docset_dic = {}
 
@@ -29,7 +36,8 @@ if __name__ == "__main__":
         print("Processing docset", docset.idCode())
         important_sentences = cs.cs(docset, compression_rate=comp_rate)  # content selection
         sent_list = io.sort_sentence_list(important_sentences)  # sort important sentences
-        content_realization = cr.ContentRealization(solver="simple")  # use simple solver in cr
+        content_realization = cr.ContentRealization(solver="ilp")  # use simple solver in cr
         content_realization.cr(sent_list, docset.idCode())
+    print("Complete!")
 
 
